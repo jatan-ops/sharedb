44142f4e92d0c94b8f0c1db7e732b71e110cdcdb
jatan-ops <jatan.shah90@gmail.com>
2022-05-03 19:09:54 +0530

    example works with react

--- examples/counter/client.js
+++ examples/counter/client.js
@@ -4,17 +4,24 @@ var sharedb = require('sharedb/lib/client');
 var socket = new ReconnectingWebSocket('ws://' + window.location.host);
 var connection = new sharedb.Connection(socket);
 
-var doc = connection.get('examples4', 'counter');
+var doc = connection.get('examples5', 'counter');
 
-doc.subscribe(showNumbers);
-doc.on('op', showNumbers);
+doc.subscribe(function() {
+  console.log('subscribed to doc')
+  showNumbers()
+});
 
-function showNumbers() {
+doc.on('op', function(op, source) {
+  // console.log('op: ', op)
+  console.log('source: ', source)
+  showNumbers()
+});
+
+function showNumbers() {  
   document.querySelector('#num-clicks').textContent = doc.data.public;
 };
 
 function increment() {
-  // const op = json1.replaceOp(['public', false, true]) // json 1
   const op = [{p: ['public'], od: doc.data.public, oi: !doc.data.public}]
   doc.submitOp(op)
 }
--- examples/counter/server.js
+++ examples/counter/server.js
@@ -1,27 +1,29 @@
 var http = require('http');
 var express = require('express');
 var ShareDB = require('sharedb');
-var WebSocket = require('ws');
 var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
+
+var WebSocket = require('ws');
 
 const db = require('sharedb-mongo')('mongodb://0.0.0.0:27017/test');
 var backend = new ShareDB({db: db});
 
 backend.use('apply', (context, next) => {
-  console.log('context: ', context.op)
+  // console.log('context: ', context.op)
   console.log('context: ', context.snapshot)
   next()
 })
 
-createDoc(startServer);
+// createDoc(startServer);
+startServer()
 
 function createDoc(callback) {
   var connection = backend.connect();
-  var doc = connection.get('examples4', 'counter');
+  var doc = connection.get('examples5', 'counter');
+  // console.log('agent: ', connection.agent)
   doc.fetch(function(err) {
     if (err) throw err;
-    if (doc.type === null) {
-      // doc.create({title: 'default title', contents: '', public: false}, json1.type.uri, callback);
+    if (doc.type === null) {      
       doc.create({title: 'default title', contents: '', public: false}, callback);
       return;
     }
@@ -37,9 +39,10 @@ function startServer() {
   var wss = new WebSocket.Server({server: server});
   wss.on('connection', function(ws) {
     var stream = new WebSocketJSONStream(ws);
-    backend.listen(stream);
+    backend.listen(stream); 
   });
 
   server.listen(8080);
   console.log('Listening on http://localhost:8080');
 }
+
